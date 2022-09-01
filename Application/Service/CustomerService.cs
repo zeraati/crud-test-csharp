e2e001cb36fe41636677b254a7a2a6030f9ee249
Zeraati <zeraatimail@gmail.com>
2022-09-01 13:02:40 +0430

    CustomerService

---
+++ Application/Service/CustomerService.cs
@@ -0,0 +1,62 @@
+﻿using DTO;
+using System.Net;
+using Entity.Model;
+using Application.Infra;
+using Application.Mapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Service
+{
+    public class CustomerService
+    {
+        private readonly ApplicationDbContext db;
+        public CustomerService(ApplicationDbContext db) => this.db = db;
+
+        public async Task<ServiceResponseDto> Add(CustomerDto dto)
+        {
+            var validation = Validation.Validator(new CustomerValidator(db.Customer),dto);
+            if (validation.StatusCode == (int)HttpStatusCode.BadRequest) return validation;
+
+            var person = CustomerMapper.Map(dto);
+            db.Customer.Add(person);
+            await db.SaveChangesAsync();
+
+            dto.Id = person.Id;
+            return new ServiceResponseDto { Message = "Added successfully." };
+        }
+
+        public async Task<ServiceResponseDto> Edit(CustomerDto dto)
+        {
+            var validation = Validation.Validator(new CustomerValidator(db.Customer), dto);
+            if (validation.StatusCode == (int)HttpStatusCode.BadRequest) return validation;
+
+            var person = await db.Customer.FirstAsync(x => x.Id == dto.Id);
+            CustomerMapper.Map(dto, person);
+            db.Customer.Update(person);
+            await db.SaveChangesAsync();
+
+            return new ServiceResponseDto { Message = "Edited successfully." };
+        }
+
+        public async Task<ServiceResponseDto> Delete(long id)
+        {
+            var person = await db.Customer.FindAsync(id);
+            db.Remove(person);
+            await db.SaveChangesAsync();
+            return new ServiceResponseDto { Message = "Deleted successfully." };
+        }
+
+        public async Task<ServiceResponseDto> GetById(long id)
+        {
+            var person = await db.Customer.FindAsync(id);
+            var dto = CustomerMapper.Map(person);
+            return new ServiceResponseDto { Data = dto };
+        }
+
+        public async Task<ServiceResponseDto> GetAll()
+        {
+            var persons = await db.Customer.Select(x => CustomerMapper.Map(x)).ToListAsync();
+            return new ServiceResponseDto { Data = persons };
+        }
+    }
+}
