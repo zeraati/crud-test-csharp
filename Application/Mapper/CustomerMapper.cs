e56b94336a651cea3d16f53aece2f5252cf3ef26
Zeraati <zeraatimail@gmail.com>
2022-09-01 11:19:29 +0430

    CustometMapper

---
+++ Application/Mapper/CustomerMapper.cs
@@ -0,0 +1,37 @@
+﻿using DTO;
+using Entity.Model;
+
+namespace Application.Mapper
+{
+    public class CustomerMapper
+    {
+        public static Customer Map(CustomerDto dto, Customer model = null)
+        {
+            model ??= new Customer();
+            model.Id = dto.Id;
+            model.FirstName = dto.Firstname;
+            model.LastName = dto.Lastname;
+            model.PhoneNumber = dto.PhoneNumber;
+            model.DateOfBirth = dto.DateOfBirth;
+            model.Email = dto.Email;
+            model.BankAccountNumber = dto.BankAccountNumber;
+            return model;
+        }
+
+        public static CustomerDto Map(Customer model)
+        {
+            return new CustomerDto
+            {
+
+                Id = model.Id,
+                Firstname = model.FirstName,
+                Lastname = model.LastName,
+                FullName = $"{model.FirstName} {model.LastName}",
+                PhoneNumber = model.PhoneNumber,
+                DateOfBirth = model.DateOfBirth,
+                Email = model.Email,
+                BankAccountNumber = model.BankAccountNumber,
+            };
+        }
+    }
+}
