e77166aa07e8452d2ddbfac133ee94d02396d66b
Zeraati <zeraatimail@gmail.com>
2022-09-01 12:53:16 +0430

    customer dto

--- DTO/DTO/CustomerDto.cs
+++ DTO/DTO/CustomerDto.cs
@@ -3,8 +3,8 @@
     public class CustomerDto
     {
         public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? PhoneNumber { get; set; }
