484661247eb3e346d0e9babdda4c7938816bba13
Zeraati <zeraatimail@gmail.com>
2022-09-01 10:32:25 +0430

    scaffold db

---
+++ DAL/DAL/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Entity.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.BankAccountNumber).HasMaxLength(50);
+
+                entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Firstname).HasMaxLength(300);
+
+                entity.Property(e => e.Lastname).HasMaxLength(300);
+
+                entity.Property(e => e.PhoneNumber).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
