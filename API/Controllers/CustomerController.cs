c38bbc8e2078e9367068e9757f7b0b1f71f665c6
Zeraati <zeraatimail@gmail.com>
2022-09-01 13:07:57 +0430

    CustomerController

---
+++ API/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+using DTO;
+using Application.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]"), ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService) => _customerService = customerService;
+
+        [HttpPost]
+        public async Task<ServiceResponseDto> Add(CustomerDto dto) => await _customerService.Add(dto);
+
+        [HttpPut]
+        public async Task<ServiceResponseDto> Edit(CustomerDto dto) => await _customerService.Edit(dto);
+
+        [HttpDelete("{id}")]
+        public async Task<ServiceResponseDto> Delete(long id) => await _customerService.Delete(id);
+
+
+        [HttpGet("{id}")]
+        public async Task<ServiceResponseDto> GetById(long id) => await _customerService.GetById(id);
+
+
+        [HttpGet("All")]
+        public async Task<ServiceResponseDto> GetAll() => await _customerService.GetAll();
+    }
+}
