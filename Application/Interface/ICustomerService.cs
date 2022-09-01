27c8c1d8782f6f7da90fd49e38ad91c1dbf49610
Zeraati <zeraatimail@gmail.com>
2022-09-01 13:07:25 +0430

    ICustomerService

---
+++ Application/Interface/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using DTO;
+
+namespace Application.Interface
+{
+    public interface ICustomerService
+    {
+        Task<ServiceResponseDto> Add(CustomerDto dto);
+        Task<ServiceResponseDto> Edit(CustomerDto dto);
+        Task<ServiceResponseDto> Delete(long id);
+        Task<ServiceResponseDto> GetById(long id);
+        Task<ServiceResponseDto> GetAll();
+    }
+}
